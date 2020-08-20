3d057f5a30560d4b39cf77a7b7e4fc0f411e76b9
estebancastroca <66756497+estebancastroca@users.noreply.github.com>
2020-08-20 10:23:00 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,4 @@
+#Plot 3
 household <- read.table("household_power_consumption.txt", header = T, sep = ";")
 #We will only be using data from the dates 2007-02-01 and 2007-02-02.
 Data1 <- household[household$Date %in% c("1/2/2007","2/2/2007") ,]
@@ -9,8 +10,10 @@ sub_Metering2 <- as.numeric(Data1$Sub_metering_2)
 sub_Metering3 <- as.numeric(Data1$Sub_metering_3)
 
 png("plot3.png", width=480, height=480)
+
 plot(datetime, sub_Metering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, sub_Metering2, type="l", col="red")
 lines(datetime, sub_Metering3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+
 dev.off()
